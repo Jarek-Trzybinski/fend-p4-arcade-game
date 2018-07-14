3c1f584b7b24617ce5f508aab9ce5551417b329c
Jaroslaw Trzybinski <jaroslaw.trzybinski@gmail.com>
2018-07-14 19:45:50 +0100

    style: change code from ES5 to ES6
    docs: write README.me

--- js/app.js
+++ js/app.js
@@ -1,118 +1,104 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-
-    //bug is moving in range and come back to start position
-    if (this.x < 500) {
-        this.x += this.speed * dt;
+class Enemy {
+    constructor(x,y, speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+         this.sprite = 'images/enemy-bug.png';
     }
-    else {
-        this.x = -100;
-    };
 
-    // checking collsion betwem player and enemies
-    if (player.x <= this.x + 70 && 
-        player.x >= this.x - 70 && 
-        player.y <= this.y + 18 && 
-        player.y >= this.y -18) {
-            player.x = 101*2;
-            player.y = ((83*4)+70);
-    };
-    
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
 
-};
+        //bug is moving in range and come back to start position
+        if (this.x < 500) {
+        this.x += this.speed * dt;
+        }
+        else {
+            this.x = -100;
+        }
 
+        // checking collsion betwem player and enemies
+        if (player.x <= this.x + 70 && 
+            player.x >= this.x - 70 && 
+            player.y <= this.y + 18 && 
+            player.y >= this.y -18) {
+                player.x = 101*2;
+                player.y = ((83*4)+70);
+        }
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y) {
-    this.x = x;
-    this.y = y;
-    this.sprite = 'images/char-boy.png'
+class Player {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/char-boy.png';
+    }
 
-};
-
-Player.prototype.update = function() {
-   
-};
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
-    if (player.y < 70) {
+    update() {
+        if (player.y < 70) {
             player.x = 101*2;
             player.y = ((83*4)+70);
-
-            /*
-            ctx.font = '40px Arial';
-            ctx.fillText('YOU HAVE WON!!!', 80, 280);
-            ctx.fillStyle = '#000';
-            */
     }
-};
-
-
-Player.prototype.handleInput = function(allowedKeys) {
-    
-    switch(allowedKeys) {
-        case 'left':
-            if (this.x - 101 >= 0) {
-                this.x -= 101;
-            }
-            break;
-            
-        case 'up':
-            //if (this.y - 83 >= 70 ) {
-                this.y -= 83;
-            //}            
-            break;
-        
-        case 'right':
-            if (this.x + 101 < 505) {
-                this.x += 101;
-            }
-            break;
-        
-        case 'down':
-            if (this.y + 83 <= 506 - 70) {
-                this.y += 83;
-            }
-            
     }
-};
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(allowedKeys) {
+        switch(allowedKeys) {
+            case 'left':
+                if (this.x - 101 >= 0) {
+                    this.x -= 101;
+                }
+                break;
+                
+            case 'up':
+                    this.y -= 83;
+                    break;
+            
+            case 'right':
+                if (this.x + 101 < 505) {
+                    this.x += 101;
+                }
+                break;
+            
+            case 'down':
+                if (this.y + 83 <= 506 - 70) {
+                    this.y += 83;
+                }
+                
+        }
+    }
+}
+
+
 // Now instantiate your objects.
+let enemy1 = new Enemy(-100, 83+70, 55);
+let enemy2 = new Enemy(-100, 70, 66);
+let enemy3 = new Enemy(-100, 83*2+70, 80);
+
 // Place all enemy objects in an array called allEnemies
-
-var enemy1 = new Enemy(-100, 83+70, 55);
-var enemy2 = new Enemy(-100, 70, 66);
-var enemy3 = new Enemy(-100, 83*2+70, 80);
 allEnemies = [enemy1, enemy2, enemy3];
+
 // Place the player object in a variable called player
-
-// 
-var player = new Player(101*2, (83*4)+70);
-
+let player = new Player(101*2, (83*4)+70);
 
 
 // This listens for key presses and sends the keys to your
